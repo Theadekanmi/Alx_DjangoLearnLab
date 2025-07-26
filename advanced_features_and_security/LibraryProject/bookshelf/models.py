83ba878019c460006df4464419604d52acb834ab
Theadekanmi <theadekanmi@gmail.com>
2025-07-26 10:21:38 -0500

    new changes

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,38 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager for CustomUser
+    """
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        
+        if not username:
+            raise ValueError('The Username field must be set')
+        
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     """
@@ -7,6 +40,9 @@ class CustomUser(AbstractUser):
     """
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    # Use the custom manager
+    objects = CustomUserManager()
     
     def __str__(self):
         return self.username
